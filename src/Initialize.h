d1edbee55935200c1bf2335f70717745a3da4386
Boris Kaus <61824822+boriskaus@users.noreply.github.com>
2022-03-18 14:13:34 +0100

    increment version

--- src/Initialize.h
+++ src/Initialize.h
@@ -161,7 +161,7 @@ global_variable global_variable_init(){
 	/* system parameters */
 
 	strcpy(gv.outpath,"./output/");				/** define the outpath to save logs and final results file	 						*/
-	strcpy(gv.version,"1.0.3 [17/03/2022]");					/** MAGEMin version 																*/
+	strcpy(gv.version,"1.0.4 [18/03/2022]");					/** MAGEMin version 																*/
 	
 	gv.len_ox           = 11;					/** number of components in the system 												*/
 	gv.max_n_cp 		= 64;					/** number of considered solution phases 											*/									
