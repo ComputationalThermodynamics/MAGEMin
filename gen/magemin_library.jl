388749c297dc1c84911304b619a6ab7eea7e1d27
Boris Kaus <61824822+boriskaus@users.noreply.github.com>
2022-03-22 09:03:48 +0100

    this will now load the locally compiled libMAGEMin.dylib if if exists, or the one from MAGEMin_jll otherwise

--- gen/magemin_library.jl
+++ gen/magemin_library.jl
@@ -1,7 +1,13 @@
 module LibMAGEMin
 
-using MAGEMin_jll
-export MAGEMin_jll
+if isfile("libMAGEMin.dylib")
+    libMAGEMin = joinpath(pwd(),"libMAGEMin.dylib")
+    println("Loading local libMAGEMin dynamic library")
+else
+    using MAGEMin_jll
+    export MAGEMin_jll
+    println("Loading libMAGEMin dynamic library from MAGEMin_jll")
+end
 
 using CEnum
 
