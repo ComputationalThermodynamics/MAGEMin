17ac0698d00d1ed66fa47f5707fc42a08e7405fb
Nicolas Riel <a.nicolas.riel@gmail.com>
2024-12-22 21:42:07 +0100

    updated version number to 1.6.3

--- src/initialize.c
+++ src/initialize.c
@@ -122,7 +122,7 @@ global_variable global_variable_alloc( bulk_info  *z_b ){
 	}
 
 	strcpy(gv.outpath,"./output/");				/** define the outpath to save logs and final results file	 						*/
-	strcpy(gv.version,"1.6.1 [28/11/2024]");	/** MAGEMin version 																*/
+	strcpy(gv.version,"1.6.3 [05/01/2025]");	/** MAGEMin version 																*/
 
 	/* generate parameters        		*/
 	strcpy(gv.buffer,"none");
